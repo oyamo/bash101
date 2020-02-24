5c8f11dfc698a5a9d6f6284590701826f19c51aa
Oyamo Brian <34618315+oyamoh-brian@users.noreply.github.com>
2020-02-24 12:20:27 +0300

    Create bash-comments.sh

---
+++ bash-comments.sh
@@ -0,0 +1,9 @@
+# every language that i know of have comments
+# to write comments in bash programming, you have to start with # symbol
+# eg
+# thiis is a comment
+# Do not excectute this it is a comment
+echo Hello World # This Code prints Hello World
+
+echo This Code has so many comments # I even commented on this code
+#Remember the text iside a comment wont be executed
